3e25a2224144e10862a4fe0d0c246fd2d4b580ec
longdahai <1741222946@qq.com>
2021-06-26 10:59:21 +0800

    添加注释

--- 1树/1二叉树的中序遍历(递归).js
+++ 1树/1二叉树的中序遍历(递归).js
@@ -1,19 +1,19 @@
-var inorderTraversal = function(root) {
+var inorderTraversal = function (root) {
 
-    if (!root) return []
-    var result = []
+  if (!root) return []
+  var result = []
 
-    function dfp(node) {
+  function dfp (node) {
 
-        if (!node) return
-        dfp(node.left)
-        result.push(node.val)
-        dfp(node.right)
+    if (!node) return
+    dfp(node.left)
+    // 中序遍历，主要查看放的位置
+    result.push(node.val)
+    dfp(node.right)
+  }
 
-
-    }
-    dfp(root)
-    return result
+  dfp(root)
+  return result
 
 
 
